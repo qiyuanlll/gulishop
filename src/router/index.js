1493917ac46e7e331dd9a9c3dfaa65b8bd38ba63
qiyuan <2593019703@qq.com>
2021-10-08 15:18:38 +0800

    se

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from "vue"
+import VueRouter  from "vue-router"
+import Home from "@/pages/Home"
+import Login from "@/pages/Login"
+import Register from "@/pages/Register"
+import Search from "@/pages/Search"
+Vue.use(VueRouter)
+export default new VueRouter({
+    routes:[
+        {
+            path:"/home",
+            component:Home
+        },
+        {
+            path:"/login",
+            component:Login
+        },
+        {
+            path:"/register",
+            component:Register
+        },
+        {
+            path:"/search",
+            component:Search
+        },
+        {
+            path:"/",
+            redirect:"/home"
+        }
+    ]
+})
